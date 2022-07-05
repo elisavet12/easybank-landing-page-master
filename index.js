3d815f571cab15eb28b54653a597f8eb9336d2df
elisavet12 <85793200+elisavet12@users.noreply.github.com>
2022-07-05 17:52:36 +0300

    almost done!

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 var burger = document.querySelector("button.burger");
 var close = document.querySelector("button.close");
 var nav = document.querySelector("nav.nav--main");
+var veil = document.querySelector("div.veil");
+var body = document.querySelector("body");
 
 function openMenu() {
   burger.style.display = "none";
   close.style.display = "block";
   nav.classList.add("nav--open");
+  veil.classList.add("veil--open");
+  veil.style.display = "block";
+  body.classList.add("noScroll");
 }
 
 function closeMenu() {
   nav.classList.remove("nav--open");
   burger.style.display = "block";
   close.style.display = "none";
+  veil.classList.remove("veil--open");
+  veil.style.display = "none";
+  body.classList.remove("noScroll");
 }
 
 burger.addEventListener("click", openMenu);
